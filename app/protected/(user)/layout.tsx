41a6aa718d5d3d3dda5e6d6ce40668ac58c51c7f
ravmax-rdl <ravindudl.rc@gmail.com>
2025-07-31 13:27:45 +0530

    refactor(layout): replace custom footer with reusable Footer component

--- app/protected/(user)/layout.tsx
+++ app/protected/(user)/layout.tsx
@@ -1,5 +1,5 @@
 
-import { ThemeSwitcher } from '@/components/ui/theme-switcher';
+import Footer from '@/components/footer';
 import { UserPanel } from '@/components/ui/user-panel';
 
 
@@ -14,9 +14,7 @@ export default function ProtectedLayout({ children }: { children: React.ReactNod
 
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5 pt-20">{children}</div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <ThemeSwitcher />
-        </footer>
+        <Footer />
       </div>
     </main>
   );
